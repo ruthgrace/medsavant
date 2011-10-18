c6d0f48e5d91fc5ee11396362ec76fea128c3f2b
Andrew Brook <abrook@@cs.toronto.edu>
2011-10-18 15:31:58 +0000

    Removing olddb
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/branches/b4@2551 096565f4-0d05-4fcd-8cbf-ff6523290f6f

---
+++ MedSavantDatabaseManager/src/org/ut/biolab/medsavant/db/util/query/QueryUtil.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ut.biolab.medsavant.db.util.query;
+
+import com.healthmarketscience.sqlbuilder.BinaryCondition;
+import com.healthmarketscience.sqlbuilder.ComboCondition;
+import com.healthmarketscience.sqlbuilder.Condition;
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbColumn;
+import org.ut.biolab.medsavant.db.model.Range;
+
+/**
+ *
+ * @author Andrew
+ */
+public class QueryUtil {
+    
+
+}
