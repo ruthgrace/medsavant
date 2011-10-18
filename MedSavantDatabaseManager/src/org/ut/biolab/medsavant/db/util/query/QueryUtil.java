3aca79c472d18f952316dd321ed546f60079d02e
Andrew Brook <abrook@@cs.toronto.edu>
2011-10-18 15:39:34 +0000

    .
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/branches/b4@2552 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- MedSavantDatabaseManager/src/org/ut/biolab/medsavant/db/util/query/QueryUtil.java
+++ MedSavantDatabaseManager/src/org/ut/biolab/medsavant/db/util/query/QueryUtil.java
@@ -8,6 +8,10 @@ import com.healthmarketscience.sqlbuilder.BinaryCondition;
 import com.healthmarketscience.sqlbuilder.ComboCondition;
 import com.healthmarketscience.sqlbuilder.Condition;
 import com.healthmarketscience.sqlbuilder.dbspec.basic.DbColumn;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.ut.biolab.medsavant.db.exception.NonFatalDatabaseException;
 import org.ut.biolab.medsavant.db.model.Range;
 
 /**
@@ -16,5 +20,32 @@ import org.ut.biolab.medsavant.db.model.Range;
  */
 public class QueryUtil {
     
+    public static List<String> getBAMFilesForDNAIds(List<String> dnaIds) throws SQLException, NonFatalDatabaseException {
+        
+        
+        /*TableSchema t = OMedSavantDatabase.getInstance().getAlignmentTableSchema();
+        SelectQuery q = new SelectQuery();
+        q.setIsDistinct(true);
+        q.addColumns(t.getDBColumn(AlignmentTableSchema.ALIAS_ALIGNMENTPATH));
+        q.addFromTable(t.getTable());
 
+        Condition[] conditions = new Condition[dnaIds.size()];
+        for(int i = 0; i < dnaIds.size(); i++){
+            conditions[i] = new BinaryCondition(BinaryCondition.Op.EQUAL_TO, t.getDBColumn(VariantTableSchema.ALIAS_DNAID), dnaIds.get(i));
+        }
+        q.addCondition(ComboCondition.or(conditions));
+        
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery(q.toString());
+
+        List<String> results = new ArrayList<String>();
+        while (rs.next()) {
+            results.add(rs.getString(1));
+        }*/
+        
+        System.err.println("NOT IMPLEMENTED YET");
+        
+        return new ArrayList<String>();      
+    }
+    
 }
