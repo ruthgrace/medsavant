eaff74c6cdef447334597a0cb0272d78bafcd8fe
Andrew Brook <abrook@@cs.toronto.edu>
2011-10-26 18:38:34 +0000

    Clear custom table map on logout.
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/branches/b4@2621 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- MedSavantDatabaseManager/src/org/ut/biolab/medsavant/db/model/structure/CustomTables.java
+++ MedSavantDatabaseManager/src/org/ut/biolab/medsavant/db/model/structure/CustomTables.java
@@ -4,13 +4,9 @@
  */
 package org.ut.biolab.medsavant.db.model.structure;
 
-import com.healthmarketscience.sqlbuilder.dbspec.basic.DbTable;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-import org.ut.biolab.medsavant.db.api.MedSavantDatabase;
-import org.ut.biolab.medsavant.db.api.MedSavantDatabase.DefaultPatientTableSchema;
-import org.ut.biolab.medsavant.db.api.MedSavantDatabase.DefaultVariantTableSchema;
 import org.ut.biolab.medsavant.db.util.DBUtil;
 
 /**
@@ -20,44 +16,6 @@ import org.ut.biolab.medsavant.db.util.DBUtil;
 public class CustomTables {
     
     private static Map<String, TableSchema> map = new HashMap<String, TableSchema>();
-    
-    /*public static class CustomvariantTableSchema extends DefaultVariantTableSchema {
-        public CustomvariantTableSchema(String tablename) {
-            super(MedSavantDatabase.schema, tablename);
-            //could add columns here...
-        }
-    }
-    
-    public static class CustompatientTableSchema extends DefaultPatientTableSchema {
-        public CustompatientTableSchema(String tablename) {
-            super(MedSavantDatabase.schema, tablename);
-            //could add columns here...
-        }
-    }
-        
-    public static TableSchema getPatientTableSchema(String tablename) {
-        
-        TableSchema table;
-        if((table = map.get(tablename)) != null){
-            return table;
-        }
-        
-        table = new CustompatientTableSchema(tablename);
-        map.put(tablename, table);
-        return table;
-    }
-    
-    public static TableSchema getVariantTableSchema(String tablename) {
-        
-        TableSchema table;
-        if((table = map.get(tablename)) != null){
-            return table;
-        }
-        
-        table = new CustomvariantTableSchema(tablename);
-        map.put(tablename, table);
-        return table;
-    }*/
 
     public static TableSchema getCustomTableSchema(String tablename) throws SQLException {
         
@@ -70,8 +28,7 @@ public class CustomTables {
         return table;
     }
     
-    public void clearMap(){
-        //TODO: do this after a logout event!!
+    public static void clearMap(){
         map.clear();
     }
     
